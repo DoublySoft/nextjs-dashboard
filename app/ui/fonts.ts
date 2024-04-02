192d157ec493b4137548ded9d4c0e15472f8916a
SrGiiL <dgilalegre96@gmail.com>
2024-04-03 01:36:37 +0200

    Chapter 5

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana, Montserrat } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
